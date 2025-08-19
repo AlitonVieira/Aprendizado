0740eedd1eba828f7248017127f844e56da59638
Áliton Vieira <alitonvieira971@gmail.com>
2025-08-18 21:17:23 -0300

    Update calculadoraIMC.py

--- Python/calculadoraIMC.py
+++ Python/calculadoraIMC.py
@@ -9,4 +9,16 @@ idade = input('Digite a sua idade: ')
 altura = float(input('Digite a sua altura: '))
 peso = float(input('Digite seu peso: '))
 imc = peso / math.pow(altura, 2)
-print(nome, 'seu IMC é de {:.2f}'.format(imc))
+
+if imc < 18.5:
+    print(nome, 'seu IMC é de {:.2f} e você está abaixo do peso, recomendo buscar um nutricionista.'.format(imc))
+elif imc < 24.9:
+    print(nome, 'seu IMC é de {:.2f} e você está com o peso normal.'.format(imc))
+elif imc < 29.9:
+    print(nome, 'seu IMC é de {:.2f} e você está com sobrepeso, recomendo buscar um nutricionista.'.format(imc))
+elif imc < 34.9:
+    print(nome, 'seu IMC é de {:.2f} e você está com obesidade grau I, recomendo buscar um nutricionista.'.format(imc))
+elif imc < 39.9:
+    print(nome, 'seu IMC é de {:.2f} e você está com obeseidade grau II, recomendo buscar um nutricionista.'.format(imc))
+else:
+    print(nome, 'seu IMC é de {:.2f} e você está com obesidade grau III(mórbida), recomendo buscar um nutricionsta.'.format(imc))
