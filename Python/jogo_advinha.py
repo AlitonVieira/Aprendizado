a4e9d7718ad1ddac5265ad162b05761f33dc3058
Áliton Vieira <alitonvieira971@gmail.com>
2025-08-24 18:39:05 -0300

    Att jogo advinha

--- Python/jogo_advinha.py
+++ Python/jogo_advinha.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 print('-' * 20)
 print('BEM VINDO AO JOGO...')
@@ -7,8 +8,10 @@ print('É só você adivinhar em que número estou pensando e você ganha!')
 print('x=' * 31)
 
 
-    
-N = int(input('Em que número estou pensando de 1 a 10? '))
+print('Estou pensando em um número de 1 a 10...')
+sleep(3.0)
+print('=' * 30)    
+N = int(input('Em que número eu pensei entre 1 e 10? '))
 number_int = random.randint(1, 10)
 print('O número que eu pensei foi {}!'.format(number_int))
 if N == number_int:
